f24992b4f2922077791445bc3409d5518e97308a
Axxxxxxy <stomioka1983@gmail.com>
2025-05-05 22:03:56 +0900

    update: apply all changes including flex-messages and response logic

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     channelSecret: process.env.LINE_CHANNEL_SECRET,
   },
   dialogflow: {
-    projectId: 'test-bot', // ✅ あなたの Dialogflow ES プロジェクトID
+    projectId: 'test-linebot-20250503', // ✅ あなたの Dialogflow ES プロジェクトID
     credentialFile: '/etc/secrets/test-linebot-20250503-0637f062f57f.json', // ✅ ファイル名を追加
     languageCode: 'ja',
   },
