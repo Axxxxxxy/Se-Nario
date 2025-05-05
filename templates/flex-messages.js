b3cc7b8de663cf5364b6af4a683f1fe7463feacb
Axxxxxxy <stomioka1983@gmail.com>
2025-05-06 01:15:53 +0900

    Fix syntax issues and unify all Flex responses

--- templates/flex-messages.js
+++ templates/flex-messages.js
@@ -1,118 +1,136 @@
-module.exports = {
-    returnMenu: {
-      type: 'bubble',
-      body: {
-        type: 'box',
-        layout: 'vertical',
-        spacing: 'md',
-        contents: [
-          {
-            type: 'text',
-            text: 'MENU',
-            size: 'sm',
-            weight: 'bold',
-            color: '#1DB446'
-          },
-          {
-            type: 'text',
-            text: 'Brand Name',
-            size: 'xxl',
-            weight: 'bold'
-          },
-          {
-            type: 'text',
-            text: '返品方法をお選びください',
-            size: 'sm',
-            color: '#888888',
-            wrap: true
-          },
-          {
-            type: 'separator',
-            margin: 'lg'
-          },
-          {
-            type: 'box',
-            layout: 'vertical',
-            spacing: 'xl',
-            margin: 'xl',
-            contents: [
-              {
-                type: 'button',
-                style: 'primary',
-                color: '#00C851',
-                action: {
-                  type: 'message',
-                  label: '店舗',
-                  text: '店舗'
-                }
-              },
-              {
-                type: 'button',
-                style: 'primary',
-                color: '#00C851',
-                action: {
-                  type: 'message',
-                  label: 'オンラインストア',
-                  text: 'オンラインストア'
-                }
+const returnMenu = {
+    type: 'bubble',
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      spacing: 'md',
+      contents: [
+        {
+          type: 'text',
+          text: '返品メニュー',
+          size: 'xl',
+          weight: 'bold',
+          color: '#1DB446',
+          wrap: true
+        },
+        {
+          type: 'text',
+          text: '返品・交換の問い合わせですね。どちらで購入いただいた商品ですか？',
+          size: 'sm',
+          color: '#888888',
+          wrap: true
+        },
+        {
+          type: 'separator',
+          margin: 'lg'
+        },
+        {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'xl',
+          margin: 'xl',
+          contents: [
+            {
+              type: 'button',
+              style: 'primary',
+              color: '#00C851',
+              action: {
+                type: 'message',
+                label: '店舗で返品',
+                text: '店舗'
               }
-            ]
-          }
-        ]
-      }
-    },
-  
-    onlineStorePrompt: {
-      type: 'bubble',
-      body: {
-        type: 'box',
-        layout: 'vertical',
-        spacing: 'md',
-        contents: [
-          {
-            type: 'text',
-            text: 'MENU',
-            size: 'sm',
-            weight: 'bold',
-            color: '#1DB446'
-          },
-          {
-            type: 'text',
-            text: 'Brand Name',
-            size: 'xxl',
-            weight: 'bold'
-          },
-          {
-            type: 'text',
-            text: 'ご注文を特定するため、以下のボタンからログインください',
-            size: 'sm',
-            color: '#888888',
-            wrap: true
-          },
-          {
-            type: 'separator',
-            margin: 'lg'
-          },
-          {
-            type: 'box',
-            layout: 'vertical',
-            spacing: 'xl',
-            margin: 'xl',
-            contents: [
-              {
-                type: 'button',
-                style: 'primary',
-                color: '#00C851',
-                action: {
-                  type: 'message',
-                  label: 'オンラインストアにログイン',
-                  text: 'オンラインストアにログイン'
-                }
+            },
+            {
+              type: 'button',
+              style: 'primary',
+              color: '#00C851',
+              action: {
+                type: 'message',
+                label: 'オンラインストアで返品',
+                text: 'オンラインストア'
               }
-            ]
-          }
-        ]
-      }
+            }
+          ]
+        }
+      ]
     }
+  };
+  
+  const onlineStorePrompt = {
+    type: 'bubble',
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      spacing: 'md',
+      contents: [
+        {
+          type: 'text',
+          text: 'オンライン返品ログイン',
+          size: 'xl',
+          weight: 'bold',
+          color: '#1DB446',
+          wrap: true
+        },
+        {
+          type: 'text',
+          text: 'ご注文を特定するため、以下のボタンからログインください。',
+          size: 'sm',
+          color: '#888888',
+          wrap: true
+        },
+        {
+          type: 'separator',
+          margin: 'lg'
+        },
+        {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'xl',
+          margin: 'xl',
+          contents: [
+            {
+              type: 'button',
+              style: 'primary',
+              color: '#00C851',
+              action: {
+                type: 'uri',
+                label: 'ログインする',
+                uri: 'https://example.com/login'
+              }
+            }
+          ]
+        }
+      ]
+    }
+  };
+  
+  const storeReturnGuide = {
+    type: 'bubble',
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          text: '店舗での返品について',
+          weight: 'bold',
+          size: 'lg',
+          wrap: true
+        },
+        {
+          type: 'text',
+          text: '購入店舗へ商品とレシートをお持ちください。詳細は公式サイトをご確認ください。',
+          size: 'sm',
+          wrap: true,
+          margin: 'md'
+        }
+      ]
+    }
+  };
+  
+  module.exports = {
+    returnMenu,
+    onlineStorePrompt,
+    storeReturnGuide
   };
   
